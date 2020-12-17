5a330e763641f4576a20c7a4e4756ec852ee93d2
Harshit Muhal <hrshtmuhal8@gmail.com>
2020-12-18 01:20:46 +0530

    configuration files

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from services import api
+from pydantic import BaseModel
+from PIL import Image
+import base64,io
+
+app =FastAPI()
+
+class Input(BaseModel):
+    encoded_img : str
+
+@app.get('/')
+def index():
+    return "Hello people, This is an API developed by Harshit Muhal for image captioning. Thanks for visiting :) :)"
+
+def get_img(encoded_img):
+    encoded_img_bytes = encoded_img.encode('utf-8')
+    base64bytes = base64.b64decode(encoded_img_bytes)
+    bytesObj = io.BytesIO(base64bytes)
+    img = Image.open(bytesObj)
+    img = img.resize((224,224))
+    return img
+
+@app.post('/predict')
+def predict(input : Input):
+    img = get_img(input.encoded_img)
+    caption=api.predict_caption(img)
+    return {"caption":caption}
