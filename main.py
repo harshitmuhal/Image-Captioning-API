5f67baaf44c612a8421f6c5534bdf70c4105e807
Harshit Muhal <hrshtmuhal8@gmail.com>
2020-12-18 17:29:34 +0530

    combined caption and main file

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from services import api
+# from services import api
 from pydantic import BaseModel
 from PIL import Image
 import base64,io,uvicorn
+
+import numpy as np
+import re, json,pickle ,keras
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing import image
+from keras.models import Model, load_model, model_from_json
+from keras.preprocessing.sequence import pad_sequences
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 app =FastAPI()
 
@@ -21,10 +30,51 @@ def get_img(encoded_img):
     img = img.resize((224,224))
     return img
 
+def predict_caption(img):
+    # Resnet model
+    model = ResNet50(weights="imagenet",input_shape=(224,224,3))
+    new_model = Model(model.input,model.layers[-2].output)
+    # vocabulary
+    with open("services/word_to_idx.pkl","rb") as f:
+        word_to_idx=pickle.load(f)
+    with open("services/idx_to_word.pkl","rb") as f:
+        idx_to_word=pickle.load(f)
+    # Main Model
+    json_file = open('services/model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    model.load_weights('services/model_weights/model_final.h5')
+    
+    # preprocessing
+    img = image.img_to_array(img)
+    img = img.reshape((1,224,224,3))
+    img = preprocess_input(img)
+    # encoding images
+    feature_vector = new_model.predict(img)
+    feature_vector = feature_vector.reshape((-1,))
+    photo=feature_vector.reshape((1,2048))
+    # Finding caption
+    in_text = '<start>'
+    max_len = 37
+    for i in range(max_len):
+        sequence = [word_to_idx[w] for w in in_text.split() if w in word_to_idx]
+        sequence = pad_sequences([sequence], maxlen=max_len,padding='post')
+        yhat = model.predict([photo,sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = idx_to_word[yhat]
+        in_text += (' ' + word)
+        if word == '<end>':
+            break
+    final = in_text.split()
+    final = final[1:-1]
+    final = ' '.join(final)
+    return final
+
 @app.post('/predict')
 def predict(input : Input):
     img = get_img(input.encoded_img)
-    caption=api.predict_caption(img)
+    caption=predict_caption(img)
     return {"caption":caption}
 
 if __name__ == '__main__':
